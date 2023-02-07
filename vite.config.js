5f81bccc0a09031654ceb28e3567de608cfd895d
Benno <bennokolar@gmail.com>
2023-02-07 08:53:15 +0100

    trying gh pages with dist attempt 3

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "TumCalculator",
+  publicPath: process.env.NODE_ENV === "production" ? "/TumCalculator/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
